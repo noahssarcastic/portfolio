0d9368c2d60e897dbb5c5a2e358788d7d7dcfab8
noahssarcastic <niniguezg@gmail.com>
2021-11-12 12:57:53 -0600

    chore: Setup react boilerplate.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,8 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-  ],
+  env: {},
+  extends: ['eslint:recommended', 'airbnb', 'prettier'],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 13,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
+  parserOptions: {},
+  plugins: ['@typescript-eslint'],
   rules: {},
 }
